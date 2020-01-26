7a7c12f26b3354bcc447babf788519bc87f99ac1
Shuchita <s.r.shuchi@gmail.com>
2020-01-26 10:23:48 +0600

    Create Find Angle MBC Solution.py

---
+++ Find Angle MBC Solution.py
@@ -0,0 +1,11 @@
+import math
+
+ab, bc = int(input()), int(input())
+
+#In, △ABC ∠B= 90∘ and Mid point = M
+#After drawing line from M to A we find a new △AMB.
+#AM = MB in △AMB [midpoint therom]
+#So, AMB is a isoceles △ and ∠MBA = ∠A
+# ∠A = tan0 =(opposite / adjacent)
+
+print("{0:.0f}°".format(math.degrees(math.atan(ab/bc))))
